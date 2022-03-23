50822cf748c805b491f6664ebc0dc1d341ff903b
blessin.varkey <blessinvarkey@gmail.com>
2022-03-23 23:45:31 +0530

    update flask files

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,24 +1,24 @@
 from flask import Flask, redirect, url_for, render_template
-import pandas as pd
-import plotly.graph_objs as go
-import plotly, json
+#import pandas as pd
+#import plotly.graph_objs as go
+#import plotly, json
 app = Flask(__name__)
 
-graph_one = [go.Scatter(
-    x = data[0],[1],
-    y = data[0],[2],
-    mode = 'lines',
-    name = country
-)]
-layout_one = dict(title = "Gender Distribution", 
-        xaxis = dict(title = '',
-        autotick=False, tick0='Male'
+#graph_one = [go.Scatter(
+#    x = data[0],[1],
+#   y = data[0],[2],
+#    mode = 'lines',
+#   name = country
+#)]
+#layout_one = dict(title = "Gender Distribution", 
+#       xaxis = dict(title = '',
+#        autotick=False, tick0='Male'
 
-        ))
+#        ))
 
 @app.route("/")
 def home():
-    return render_template('index.html', dataset = data)
+    return render_template('index.html')
 
 
 
